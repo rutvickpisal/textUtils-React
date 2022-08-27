f16354a918c8e390dabf7d8a44733e16741ac376
rutvickpisal <48404451+rutvickpisal@users.noreply.github.com>
2022-08-27 09:09:17 +0530

    Add files via upload

---
+++ build/precache-manifest.2c9e955b901b3d31cdde27e0bfd1e576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46fd2afa2d26610ad291",
+    "url": "/static/css/main.e5944e08.chunk.css"
+  },
+  {
+    "revision": "46fd2afa2d26610ad291",
+    "url": "/static/js/main.46fd2afa.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "314f00da2ddf04ba896a",
+    "url": "/static/js/2.314f00da.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "2723545405af467912ba5caac01b9227",
+    "url": "/index.html"
+  }
+];
